ba9f7185b5fbf4ad007d1b572158ee00cc275b08
XL_Mr <643158351@qq.com>
2022-08-01 14:42:42 +0800

    完成了项目初始化

---
+++ src/components/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button>提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  created () {
+
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
